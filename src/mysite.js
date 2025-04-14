7a03c4bfe91b43ee93338dfb8eb2e6569d1b2940
adhithyansn <adhithyansngit@gmail.com>
2025-04-14 13:00:19 +0530

    c

--- src/mysite.js
+++ src/mysite.js
@@ -95,7 +95,7 @@ const Portfolio = () => {
         hoverIntensity={hoverIntensity} 
         enableHover={enableHover}
       >
-        200
+        777
       </FuzzyText>
       <h1 className="text-5xl md:text-6xl font-bold mb-6 bg-gradient-to-r from-yellow-500 to-yellow-200 bg-clip-text text-transparent whitespace-nowrap">
         Adhithyan SN
